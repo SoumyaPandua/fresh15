b77644f6bd9f16253ac1002c727bea41f979fe0b
axyz10649 <axyz10649@gmail.com>
2026-08-02 22:04:01 +0530

    profile module update

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,7 +1,12 @@
 import { v2 as cloudinary } from "cloudinary";
 import streamifier from "streamifier";
+import "./env.js";
 
-// your existing cloudinary.config({...})
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export const uploadImage = (buffer, folder) => {
   return new Promise((resolve, reject) => {
